57250557b65dcc816e499e4e696c04fe6d4fbc60
OlgaLe <lebedevaolga021@gmail.com>
2019-08-05 17:53:02 +0300

    Lecture6 fix

--- Lecture6/src/test/java/TestOzBy.java
+++ Lecture6/src/test/java/TestOzBy.java
@@ -10,34 +10,33 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TestOzBy {
     private WebDriver driver;
-    private final String PROPERTY_DRIVER = "webdriver.chrome.driver";
-    private final String DRIVER_EXE = "chromedriver.exe";
-    private final String URL = "https://www.oz.by";
-    private final String MY_EMAIL = "lebedevaolga021@gmail.com";
-    private final String MY_PASSWORD = "wPdAp9";
-    private final String BOOK_NAME = "Нейромант";
-    private final long TIMEOUT = 1;
-    private final String LOGIN_BAR = "top-panel__userbar__auth";
-    private final String LOGIN_BY_EMAIL = "loginFormLoginEmailLink";
-    private final String EMAIL_FIELD = "cl_email";
-    private final String PASSWORD_FIELD = "//*[@name=\"cl_psw\"]";
-    private final String LOGIN_CLICK = "#loginForm > button";
-    private final String TOP_SEARCH_BAR = "top-s";
-    private final String CLICK_SEARCH = "top-panel__search__btn";
-    private final String CLICK_ON_ELEMENT = "item-type-card";
-    private final String CLICK_ON_BASKET = "i-button__icon";
-    private final String CHECK_BASKET = "//a[contains(.,'Корзина')]";
+    private static final String PROPERTY_DRIVER = "webdriver.chrome.driver";
+    private static final String DRIVER_NAME = "chromedriver.exe";
+    private static final String URL = "https://www.oz.by";
+    private static final String MY_EMAIL = "lebedevaolga021@gmail.com";
+    private static final String MY_PASSWORD = "wPdAp9";
+    private static final String BOOK_NAME = "Нейромант";
+    private static final long TIMEOUT = 1;
+    private static final String LOGIN_BAR = "top-panel__userbar__auth";
+    private static final String LOGIN_BY_EMAIL = "loginFormLoginEmailLink";
+    private static final String EMAIL_FIELD = "cl_email";
+    private static final String PASSWORD_FIELD = "//*[@name=\"cl_psw\"]";
+    private static final String LOGIN_CLICK = "#loginForm > button";
+    private static final String TOP_SEARCH_BAR = "top-s";
+    private static final String CLICK_SEARCH = "top-panel__search__btn";
+    private static final String CLICK_ON_ELEMENT = "item-type-card";
+    private static final String CLICK_ON_BASKET = "i-button__icon";
+    private static final String CHECK_BASKET = "//a[contains(.,'Корзина')]";
 
 
     @BeforeClass
     public void beforeClass() {
-        System.setProperty(PROPERTY_DRIVER, DRIVER_EXE);
+        System.setProperty(PROPERTY_DRIVER,DRIVER_NAME );
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
     }
@@ -53,7 +52,7 @@ public class TestOzBy {
         loginToSite();
         searchBook(BOOK_NAME);
         putInBasket();
-        checkBasket();
+        checkBasket(getListOfBooks(), BOOK_NAME);
     }
 
     public void loginToSite() {
@@ -77,14 +76,15 @@ public class TestOzBy {
 
     }
 
-    public void checkBasket() {
+    public List<WebElement> getListOfBooks() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CHECK_BASKET))).click();
         List<WebElement> allBooksLinks = driver.findElements(By.xpath(CHECK_BASKET));
-        Iterator<WebElement> listOfBookInBasket = allBooksLinks.iterator();
-        while (listOfBookInBasket.hasNext()) {
-            listOfBookInBasket.next();
-            Assert.assertTrue(true, BOOK_NAME);
-        }
+        return allBooksLinks;
+    }
+
+    public void checkBasket(List<WebElement> allBooksLinks, String bookName) {
+        allBooksLinks.stream().filter(b -> bookName.equals(b));
+        Assert.assertTrue(true, bookName);
     }
 }
