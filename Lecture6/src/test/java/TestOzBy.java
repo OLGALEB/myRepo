b54a8b47b2b96c560f5f3b28fd6caf66db2d2e8c
OlgaLe <lebedevaolga021@gmail.com>
2019-08-06 11:24:09 +0300

    Lecture6 fix

--- Lecture6/src/test/java/TestOzBy.java
+++ Lecture6/src/test/java/TestOzBy.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,6 +11,9 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestOzBy {
     private WebDriver driver;
@@ -20,7 +22,7 @@ public class TestOzBy {
     private static final String URL = "https://www.oz.by";
     private static final String MY_EMAIL = "lebedevaolga021@gmail.com";
     private static final String MY_PASSWORD = "wPdAp9";
-    private static final String BOOK_NAME = "Нейромант";
+    private static final String BOOK_NAME = "Нейромант (м)";
     private static final long TIMEOUT = 1;
     private static final String LOGIN_BAR = "top-panel__userbar__auth";
     private static final String LOGIN_BY_EMAIL = "loginFormLoginEmailLink";
@@ -32,11 +34,12 @@ public class TestOzBy {
     private static final String CLICK_ON_ELEMENT = "item-type-card";
     private static final String CLICK_ON_BASKET = "i-button__icon";
     private static final String CHECK_BASKET = "//a[contains(.,'Корзина')]";
+    private static final String BOOK_IN_BASKET = "//a[contains(.,'Нейромант (м)')]";
 
 
     @BeforeClass
     public void beforeClass() {
-        System.setProperty(PROPERTY_DRIVER,DRIVER_NAME );
+        System.setProperty(PROPERTY_DRIVER, DRIVER_NAME);
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
     }
@@ -52,7 +55,7 @@ public class TestOzBy {
         loginToSite();
         searchBook(BOOK_NAME);
         putInBasket();
-        checkBasket(getListOfBooks(), BOOK_NAME);
+        checkBasket(BOOK_NAME);
     }
 
     public void loginToSite() {
@@ -76,15 +79,14 @@ public class TestOzBy {
 
     }
 
-    public List<WebElement> getListOfBooks() {
+    public List<String> getListOfBooks() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CHECK_BASKET))).click();
-        List<WebElement> allBooksLinks = driver.findElements(By.xpath(CHECK_BASKET));
+        List<String> allBooksLinks = driver.findElements(By.xpath(BOOK_IN_BASKET)).stream().map(WebElement::getText).collect(Collectors.toList());
         return allBooksLinks;
     }
 
-    public void checkBasket(List<WebElement> allBooksLinks, String bookName) {
-        allBooksLinks.stream().filter(b -> bookName.equals(b));
-        Assert.assertTrue(true, bookName);
+    public void checkBasket(String bookName) {
+        assertThat(getListOfBooks()).hasSize(1).contains(bookName);
     }
 }
